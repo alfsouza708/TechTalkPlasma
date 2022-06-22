9ad6c839c13235f3df60277cc667fe8ab27b4666
Antonio Souza <alfsouza708@gmail.com>
2022-06-22 13:59:56 -0300

    feat(setup): initial setup for example
    initial setup for future branches

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,71 @@
+<script setup lang="ts"></script>
+
 <template>
-	<div class="home-screen">
-		<div class="content">
-			<div class="presentation">
-				<img src="@img/plasma.svg" alt="logo" />
-			</div>
-			<div class="summary">
-				<h2 class="welcome">
-					{{ $t('plasma_welcome') }}
-					<a
-						class="learn"
-						target="__blank"
-						href="https://github.com/solfacil/plasma/wiki"
-					>
-						{{ $t('learn_plasma') }}
-					</a>
-				</h2>
-			</div>
-		</div>
+	<div id="content">
+		<!-- <form v-if="state.isEdit" @submit.prevent="">
+			<SolTextfield id="title" v-model="editTodo.title" label="title" invert />
+			<SolTextfield
+				id="description"
+				v-model="editTodo.description"
+				label="description"
+				invert
+			/>
+			<SolButton id="insert" type="submit"> $t('edit') </SolButton>
+		</form> -->
+
+		<table>
+			<thead>
+				<th>Id</th>
+				<th>$t('title')</th>
+				<th>$t('description')</th>
+			</thead>
+			<ClientOnly>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</ClientOnly>
+		</table>
 	</div>
 </template>
 
-<script setup lang="ts"></script>
-
 <style scoped lang="scss">
-.home-screen {
-	@apply w-screen max-w-full bg-neutral-low-pure;
+#content {
+	@apply md:flex justify-evenly;
 
-	> .content {
-		@apply flex justify-center flex-col bg-neutral-high-medium rounded-sm p-sm drop-shadow-sm shadow-neutral-high-light;
+	table {
+		@apply text-neutral-high-pure flex-auto;
+		margin: auto;
 
-		.presentation {
-			img {
-				@apply h-40 w-80;
-				margin: auto;
+		thead {
+			@apply border-neutral-high-light border-sm text-center;
+
+			> * {
+				@apply border-neutral-high-light border-sm text-center;
 			}
 		}
 
-		.summary {
-			@apply mb-3xs;
-			.welcome {
-				@apply font-sans font-medium text-center;
+		tbody {
+			> * {
+				@apply border-neutral-high-light border-sm text-center;
+			}
 
-				.learn {
-					@apply text-feedback-informative-pure underline;
+			td {
+				@apply border-neutral-high-light border-sm;
+
+				> * {
+					margin: auto;
 				}
 			}
 		}
+	}
+
+	form {
+		@apply flex-auto flex flex-col w-fit-content gap-4 p-micro rounded-md items-center;
+		margin: auto;
 	}
 }
 </style>
